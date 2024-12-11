c5160dda4a8c63d84911b5a6ef0665bb0e763597
Lairton <130435095+Laiirton@users.noreply.github.com>
2024-12-10 21:41:03 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,31 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { createGlobalStyle } from 'styled-components';
 import App from "./App";
-import GlobalStyles from "./styles/GlobalStyles";
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  body {
+    margin: 0;
+    padding: 0;
+    font-family: 'Inter', -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+      'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+      sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    background: #1a1b1e;
+    color: #ffffff;
+  }
+`;
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <GlobalStyles />
+    <GlobalStyle />
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
 );
